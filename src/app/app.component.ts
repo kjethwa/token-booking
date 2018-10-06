3d6fded966af3de57dc67d8d1c0d997c791221dd
kalpesh <kalpeshjethwa92@gmail.com>
2018-10-06 12:28:29 +0530

    Commit from feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'token-booking';
+  title = 'token-booking feature branch';
 }
